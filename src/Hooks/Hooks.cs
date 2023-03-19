fe2265e6167251ff2ea69cf91f01de6253708106
Tony p <missionant@hotmail.co.uk>
2023-03-19 22:27:51 +0000

    Removed comment

--- src/Hooks/Hooks.cs
+++ src/Hooks/Hooks.cs
@@ -6,7 +6,7 @@ using TechTalk.SpecFlow;
 namespace MyBillAfricaDraft2.Hooks
 {
     [Binding]
-    public sealed class Hooks //this comment is just a test
+    public sealed class Hooks 
     {
         private readonly IObjectContainer _container;
         public Hooks(IObjectContainer container)
